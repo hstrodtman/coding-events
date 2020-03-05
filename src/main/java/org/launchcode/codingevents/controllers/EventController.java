478e539a0bf9d6ff02125e3776dc815ddda49213
hstrodtman <heatherstrodtman@gmail.com>
2020-03-05 16:07:03 -0600

    Added dependencies & updated application properties to connect the coding_events MySQL database. Created an EventRepository interface & updated Controllers to use it.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,25 +1,27 @@
 package org.launchcode.codingevents.controllers;
 
-import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 import org.launchcode.codingevents.models.EventType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    @Autowired
+    private EventRepository eventRepository;
+
     @GetMapping
     public String displayEvents(Model model){
        model.addAttribute("title", "All Events");
-       model.addAttribute("events", EventData.getAll());
+       model.addAttribute("events", eventRepository.findAll());
         return "events/index";
     }
 
@@ -35,17 +37,18 @@ public class EventController {
     public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
         if(errors.hasErrors()){
             model.addAttribute("title", "Add Event");
+            model.addAttribute("eventTypes", EventType.values());
             return "events/create";
         }
 
-        EventData.add(newEvent);
+        eventRepository.save(newEvent);
         return "redirect:";
     }
 
     @GetMapping("delete")
     public String renderDeleteEventForm(Model model){
         model.addAttribute("title", "Delete Events");
-        model.addAttribute("events", EventData.getAll());
+        model.addAttribute("events", eventRepository.findAll());
         return "events/delete";
     }
 
@@ -53,7 +56,7 @@ public class EventController {
     public String deleteEvent(@RequestParam(required = false) int[] eventIds){
         if(eventIds !=null){
             for (int id : eventIds){
-                EventData.remove(id);
+                eventRepository.deleteById(id);
             }
         }
         return "redirect:";
