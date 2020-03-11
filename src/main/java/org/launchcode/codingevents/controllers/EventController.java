79ebfe9747446f0b028259038b3f6f910d0fc684
hstrodtman <heatherstrodtman@gmail.com>
2020-03-11 08:21:24 -0500

    Updating controllers for persistent relationship between event & EventCategory.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,9 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
-import org.launchcode.codingevents.models.EventType;
+import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("events")
@@ -18,10 +20,24 @@ public class EventController {
     @Autowired
     private EventRepository eventRepository;
 
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
     @GetMapping
-    public String displayEvents(Model model){
-       model.addAttribute("title", "All Events");
-       model.addAttribute("events", eventRepository.findAll());
+    public String displayEvents(@RequestParam(required =false) Integer categoryId, Model model){
+        if (categoryId == null){
+            model.addAttribute("title", "All Events");
+            model.addAttribute("events", eventRepository.findAll());
+        } else {
+            Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
+            if (result.isEmpty()){
+                model.addAttribute("title", "Invalid Category ID: " + categoryId);
+            } else {
+                EventCategory category= result.get();
+                model.addAttribute("title", "Events in category: " + category.getName());
+                model.addAttribute("events", category.getEvents());
+            }
+        }
         return "events/index";
     }
 
@@ -29,7 +45,7 @@ public class EventController {
     public String renderCreateEventForm(Model model){
         model.addAttribute("title", "Add Event");
         model.addAttribute("event", new Event());
-        model.addAttribute("eventTypes", EventType.values());
+        model.addAttribute("categories", eventCategoryRepository.findAll());
         return "events/create";
     }
 
@@ -37,7 +53,6 @@ public class EventController {
     public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
         if(errors.hasErrors()){
             model.addAttribute("title", "Add Event");
-            model.addAttribute("eventTypes", EventType.values());
             return "events/create";
         }
         eventRepository.save(newEvent);
--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,24 +1,25 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import java.util.Objects;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class EventCategory extends AbstractEntity{
 
-    @NotBlank
+    @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
+
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
 
     public EventCategory(String name){
         this.name = name;
     }
 
-    public EventCategory(){
-
-    }
+    public EventCategory(){    }
 
     public String getName() {
         return name;
@@ -28,4 +29,12 @@ public class EventCategory extends AbstractEntity{
         this.name = name;
     }
 
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
